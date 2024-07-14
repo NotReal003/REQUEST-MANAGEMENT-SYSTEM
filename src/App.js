1340fae90a7a8cfc603bb372cea149f0490460e5
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-07-14 20:03:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import ReportForm from './pages/ReportForm';
+import Support from './pages/Support';
+import Apply from './pages/Apply';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container mx-auto p-4">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/report" component={ReportForm} />
+            <Route path="/support" component={Support} />
+            <Route path="/apply" component={Apply} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
