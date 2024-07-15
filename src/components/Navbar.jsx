5b8fdcf828650638d4247a18f9a53a1997efbc18
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-07-15 13:57:23 +0000

    G

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <Link className="btn btn-ghost normal-case text-xl" to="/">Example</Link>
+        <Link className="btn btn-ghost normal-case text-xl" to="/">Example!!</Link>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal p-0">
