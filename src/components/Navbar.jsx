cb1f0fef5a775d03832aba9daf6cd3de13849d1f
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-07-15 21:26:22 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,18 +5,25 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <Link className="btn btn-ghost normal-case text-xl" to="/">Example!!</Link>
-      </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal p-0">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/support">Support</Link></li>
-          <li><Link to="/apply">Apply</Link></li>
-          <li><Link to="/report">Report</Link></li>
-        </ul>
-      </div>
-    </div>
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl">NotReal003 | Requests</a>
+  </div>
+  <div className="flex-none">
+    <ul className="menu menu-horizontal px-1">
+      <li><Link to="/">Home</Link></li>
+      <li>
+        <details>
+          <summary>Requests</summary>
+          <ul className="bg-base-100 rounded-t-none p-2">
+            <li><Link to="/apply">Apply</Link></li>
+            <li><Link to="/report">/Report</Link></li>
+            <li><Link to="/support">Support</Link></li>
+          </ul>
+        </details>
+      </li>
+    </ul>
+  </div>
+</div>
   );
 };
 
