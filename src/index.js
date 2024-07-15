d775aa6b40ed882d316dfa1766b2cef82555a4fc
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-07-15 23:57:12 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Import Tailwind CSS
+import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
