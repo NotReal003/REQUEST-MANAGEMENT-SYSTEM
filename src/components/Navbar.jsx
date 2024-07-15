ff4727d4caa9a16450c322143efa80e1db36a428
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-07-16 01:30:42 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,17 +28,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-    <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <Link className="btn btn-ghost normal-case text-xl" to="/">Example!!</Link>
-      </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal p-0">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/support">Support</Link></li>
-          <li><Link to="/apply">Apply</Link></li>
-          <li><Link to="/report">Report</Link></li>
-        </ul>
-      </div>
-    </div>
