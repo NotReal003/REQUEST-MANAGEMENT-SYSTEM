b608639e2dadd91e4db8550e13999282b6ba4e3d
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-07-16 16:50:08 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
-    <footer className="footer footer-center bg-base-200 text-base-content rounded p-10 mt-35">
+    <footer className="footer footer-center bg-base-200 text-base-content rounded p-10 mt-40">
   <nav className="grid grid-flow-col gap-4">
     <Link to="https://notreal003.xyz/contact" className="link link-hover hover:underline">Contact Us</Link>
     <Link to="https://support.notreal003.xyz/privacy" className="link link-hover hover:underline">Privacy Policy</Link>
