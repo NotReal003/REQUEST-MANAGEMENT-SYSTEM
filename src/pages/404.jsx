95b5e56df35287beab0b07937b467bdf433c07b5
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-07-16 14:43:11 +0530

    Update 404.jsx

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -9,6 +9,8 @@ const NotFound = () => {
         <h1 className="text-4xl font-bold mb-4">404 - Not Found</h1>
         <p className="mb-4">The page you are looking for does not exist.</p>
         <Link to="/" className="btn btn-primary">Home</Link>
+        <Link to="https://notreal003.xyz" className="btn mt=5 p=10">NotReal003</Link>
+        <Link to="https://suppport.notreal003.xyz" className="btn">Support HelpDesk</Link>
       </div>
     </div>
   );
