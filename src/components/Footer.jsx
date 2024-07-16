9e080fc6802fb76577af9cc26801c37fc71bfb55
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-07-16 11:27:30 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,83 +1,43 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-
-const Navbar = () => {
-  const [theme, setTheme] = useState('light');
-
-  useEffect(() => {
-    if (theme === 'dark') {
-      document.documentElement.setAttribute('data-theme', 'dark');
-    } else {
-      document.documentElement.setAttribute('data-theme', 'light');
-    }
-  }, [theme]);
-
-  const handleThemeChange = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
-  };
-
+const Footer = () => {
   return (
-    <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <Link className="btn btn-ghost text-xl" to="/">NotReal003</Link>
-      </div>
-      <label className="swap swap-rotate">
-        {/* this hidden checkbox controls the state */}
-        <input 
-          type="checkbox" 
-          className="theme-controller" 
-          checked={theme === 'dark'} 
-          onChange={handleThemeChange} 
-        />
-
-        {/* sun icon */}
+    <footer className="footer footer-center bg-base-200 text-base-content rounded p-10">
+  <nav className="grid grid-flow-col gap-4">
+    <Link to="https://support.notreal003.xyz/contact" className="link link-hover">Contact Us</Link>
+    <Link to="https://support.notreal003.xyz/privacy" className="link link-hover">Privacy Policy</Link>
+  </nav>
+  <nav>
+    <div className="grid grid-flow-col gap-4">
+      <Link to="https://x.com/NotNT77">
         <svg
-          className="swap-off h-6 w-6 fill-current flex items-center justify-center"
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24">
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          className="fill-current">
           <path
-            d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+            d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
         </svg>
-
-        {/* moon icon */}
+      </Link>
+      <Link to="https://youtube.com/@notnt77?si=VYMvcWcwVCnpeZGL">
         <svg
-          className="swap-on h-5 w-5 fill-current flex items-center justify-center"
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24">
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          className="fill-current">
           <path
-            d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+            d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
         </svg>
-      </label>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1 ml-1">
-          <li>
-            <Link to="https://notreal003.xyz">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-              </svg>
-            </Link>
-          </li>
-          <div className="dropdown dropdown-bottom dropdown-end">
-            <div tabIndex={0} role="button" className="btn m-1">Requests</div>
-            <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-              <li><Link to="/apply">Apply</Link></li>
-              <li><Link to="/Report">Report</Link></li>
-              <li><Link to="/support">Support</Link></li>
-            </ul>
-          </div>
-        </ul>
-      </div>
+      </Link>
     </div>
-  );
+  </nav>
+  <aside>
+    <p>Copyright © 2024 NotReal003 - All rights reserved.</p>
+  </aside>
+</footer>
+      );
 };
 
-export default Navbar;
+export default Footer;
