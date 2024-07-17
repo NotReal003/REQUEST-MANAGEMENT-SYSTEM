bcebec89b29c6ba15c474185d9884006eed1e751
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-07-17 15:10:14 +0000

    Fotter bottom

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-// src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import ReportForm from './pages/ReportForm';
 import Support from './pages/Support';
 import Apply from './pages/Apply';
-import NotFound from './pages/404';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
     <Router>
-      <div className="App">
+      <div className="flex flex-col min-h-screen">
         <Navbar />
-        <div className="container mx-auto p-4">
+        <div className="flex-grow container mx-auto p-4">
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route path="/report" element={<ReportForm />} />
@@ -22,6 +22,7 @@ const App = () => {
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
+        <Footer />
       </div>
     </Router>
   );
