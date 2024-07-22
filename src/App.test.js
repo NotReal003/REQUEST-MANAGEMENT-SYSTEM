3a217229f4d167d462d3976729b454e6c48b6dbf
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-07-22 18:07:30 +0000

    Fix

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@
 
 // test('renders learn react link', () => {
 //  render(<App />);
-//  const linkElement = screen.getByText(/learn react/i);
+//  const linkElement = screen.getByText(/dududuulearn react/i);
 //  expect(linkElement).toBeInTheDocument();
 //});
