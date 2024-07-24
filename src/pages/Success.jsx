5f53d6728bd928cf3e369bcdc5e8f25320631817
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-07-24 11:48:49 +0000

    Suc

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { CircleCheck, House } from 'lucide-react';
+import { FaDiscord } from "react-icons/fa6";
 const Success = () => {
   return (
     <div className="flex items-center justify-center bg-base-50">
@@ -13,6 +14,7 @@ const Success = () => {
         <Link to="/" className="btn btn-outline btn-info mt-4 w-full">
           <House className='size-4'/> Back to Home
         </Link>
+        <a target="_blank" href="https://discord.gg/sqVBrMVQmp" className="btn btn-outline btn-primary w-full mt-5"><FaDiscord /> Join our Discord Server</a>
       </div>
     </div>
   );
