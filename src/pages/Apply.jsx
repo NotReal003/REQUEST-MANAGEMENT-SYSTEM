0fe5e010021e52f5eae09bb74fade637a81f6885
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-07-24 10:58:53 +0000

    Fixs

--- src/pages/Apply.jsx
+++ src/pages/Apply.jsx
@@ -1,11 +1,16 @@
 // src/pages/Apply.jsx
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { House } from 'lucide-react';
 
 const Apply = () => {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Guild Application</h1>
       <p>Comming soon!</p>
+      <Link to="/" className="btn btn-outline btn-info mt-4 w-full">
+        <House className='size-4'/> Back to Home
+      </Link>
     </div>
   );
 };
