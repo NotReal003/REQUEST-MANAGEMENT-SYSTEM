262c13532c94e3c8e191c951a308f68b3b7e25ba
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-08-11 18:45:04 +0000

    History fix

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useHistory, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 
