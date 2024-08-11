5ee5b086043e16d9200a398a9dc103f6b97fa7dd
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-08-11 10:58:59 +0000

    A fix

--- src/pages/ReportForm.jsx
+++ src/pages/ReportForm.jsx
@@ -57,7 +57,7 @@ const ReportForm = () => {
         <h1 className="text-2xl font-bold mb-4 fill-current flex items-center justify-center">
           <TbMessageReport className="size-6 mr-2"/>Discord report
         </h1>
-        {status && <div className="mt-4 alert alert-error mb-4">{status}</div>}
+        {status && <div className="mt-4 alert alert-warning mb-4">{status}</div>}
         <div role="alert" className="alert">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info h-6 w-6 shrink-0">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
