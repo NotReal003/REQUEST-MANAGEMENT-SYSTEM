00eadc515aa92fcdbbd96c44dc2c85d461f3ddd7
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-08-12 12:31:01 +0000

    Skeleton

--- src/pages/RequestDetail.jsx
+++ src/pages/RequestDetail.jsx
@@ -24,7 +24,12 @@ axios.get(`https://api.notreal003.xyz/requests/${requestId}`, {
   }, [requestId]);
 
   if (!request) {
-    return <div><span className="loading loading-spinner loading-md"></span></div>;
+    return <div className="flex w-52 flex-col gap-4">
+        <div className="skeleton h-32 w-full"></div>
+        <div className="skeleton h-4 w-28"></div>
+        <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton h-4 w-full"></div>
+      </div>;
   }
 
   return (
