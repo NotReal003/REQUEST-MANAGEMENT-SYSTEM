4a1f66f29cdf0d6733fb134936fcfdd79326649f
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-08-13 09:32:16 +0000

    Fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,18 +3,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import { FaDiscord } from 'react-icons/fa';
-import { MdOutlineSupportAgent, MdOutlineSecurity } from "react-icons/md";
-import { SiFormstack } from "react-icons/si";
+import { MdSupportAgent } from "react-icons/md";
+import { IoMdMail } from "react-icons/io";
+import { IoShieldCheckmark } from "react-icons/io5";
 
 const Home = () => {
   return (
     <div className="container mx-auto p-4 h-5">
       <h1 className="text-2xl font-bold mb-6">Requests</h1>
       <div className="space-y-6 mt-4">
-          <Link to="/one" className="btn btn-outline btn-warning w-full"><SiFormstack />Your Requests</Link>
-        <Link to="/report" className="btn btn-outline btn-info w-full"><FaDiscord />Discord Report</Link>
-        <Link to="/apply" className="btn btn-outline btn-secondary w-full"><MdOutlineSecurity /> Guild Application</Link>
-        <Link to="/support" className="btn btn-outline btn-accent w-full"><MdOutlineSupportAgent />Support Request</Link>
+          <Link to="/one" className="btn btn-outline btn-info w-full"><IoShieldCheckmark />Your Requests</Link>
+        <Link to="/report" className="btn btn-outline btn-warning w-full"><FaDiscord />Discord Report</Link>
+        <Link to="/apply" className="btn btn-outline btn-secondary w-full"><IoMdMail /> Guild Application</Link>
+        <Link to="/support" className="btn btn-outline btn-accent w-full"><MdSupportAgent />Support Request</Link>
       </div>
       <Footer />
     </div>
--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { CircleCheck, House } from 'lucide-react';
 import { FaDiscord } from "react-icons/fa6";
+import { IoShieldCheckmark } from "react-icons/io5";
 const Success = () => {
   return (
     <div className="flex items-center justify-center bg-base-50">
@@ -11,10 +12,13 @@ const Success = () => {
         </div>
         <h1 className="text-2xl font-bold mb-2">Success!</h1>
         <p className="text-lg">Your request was submitted successfully, join our Discord Server so we may contact you :)</p>
-        <Link to="/" className="btn btn-outline btn-info mt-4 w-full">
-          <House className='size-4'/> Back to Home
+        <Link to="/one" className="btn btn-outline btn-info mt-4 w-full">
+          <IoShieldCheckmark className='size-4'/> Your Requests
         </Link>
         <a target="_blank" href="https://discord.gg/sqVBrMVQmp" className="btn btn-outline btn-primary w-full mt-5"><FaDiscord /> Join our Discord Server</a>
+        <Link to="/" className="btn btn-outline btn-info mt-4 w-full">
+          <House className='size-4'/> Back to Home Page
+        </Link>
       </div>
     </div>
   );
