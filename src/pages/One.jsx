aae39aa8e3808ef21db8319c9c4390f984f0b888
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-08-13 20:31:20 +0000

    Fix

--- src/pages/One.jsx
+++ src/pages/One.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-  import { Link, useNavigate } from 'react-router-dom';;
+import { Link, useNavigate } from 'react-router-dom';;
 import { FaDiscord, FaArrowRight } from 'react-icons/fa';
 import { MdSupportAgent } from 'react-icons/md';
 
