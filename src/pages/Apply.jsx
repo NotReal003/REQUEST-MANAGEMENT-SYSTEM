5abff07a2853786c37a75c85573d6c43f4c5bee1
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-08-13 15:30:46 +0000

    Update

--- src/pages/Apply.jsx
+++ src/pages/Apply.jsx
@@ -2,11 +2,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { House } from 'lucide-react';
+import { BiMessageRoundedX } from "react-icons/bi";
 
 const Apply = () => {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Guild Application</h1>
+      <h1 className="text-2xl font-bold mb-4">Guild Application <BiMessageRoundedX className="size-4 mr-2 ml-2"</h1>
       <p>Comming soon!</p>
       <Link to="/" className="btn btn-outline btn-info mt-4 w-full">
         <House className='size-4'/> Back to Home
