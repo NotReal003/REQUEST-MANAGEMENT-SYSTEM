ec2764464cd0b03c15629885eeb54f7e0025b0bb
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-08-14 13:09:43 +0000

    Fix

--- src/pages/AdminDetail.jsx
+++ src/pages/AdminDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 function AdminDetail() {
@@ -8,6 +8,7 @@ function AdminDetail() {
   const [alert, setAlert] = useState(null);
   const [status, setStatus] = useState('');
   const [reviewMessage, setReviewMessage] = useState('');
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchRequest = async () => {
