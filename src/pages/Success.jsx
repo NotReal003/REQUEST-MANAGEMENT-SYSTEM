75035a5e1eac81117126680c59b6dbd916341d90
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-08-17 13:22:02 +0000

    G

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -32,7 +32,7 @@ const Success = () => {
         setRequest(requestData);
       } catch (error) {
         console.error('Error fetching request details:', error);
-        setError(requestData.message || 'Failed to load request.');
+        setError(error.message || 'Failed to load request!');
       } finally {
         setLoading(false);
       }
