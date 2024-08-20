7ea87011c00dbfb2e54193a18d391885a5b672cb
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-08-20 11:22:41 +0530

    Update Support.jsx

--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -68,7 +68,7 @@ const Support = () => {
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info h-6 w-6 shrink-0">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
           </svg>
-          <span>Feel free ask anything!</span>
+          <span>Feel free ask anything! if you are submitting guild application, then let us know it is Guild application and provide us with your In-Game Name.</span>
         </div>
         <form id="reportForm" onSubmit={handleSubmit}>
           <label htmlFor="messageLink" className="label">Your support request (required)</label>
