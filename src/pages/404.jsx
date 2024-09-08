9bc2a70394850512da9f0d375c658a95b3661b1c
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-09-08 10:41:38 +0000

    Fix

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -6,10 +6,9 @@ import { FaRegFaceGrinBeamSweat } from "react-icons/fa6";
 const NotFound = () => {
   return (
     <div className="container mx-auto p-4">
-      <div className="form-container min-h-screen text-center items-center">
+      <div className="form-container min-h-screen text-center flex items-center gap-2">
         <h1 className="text-4xl font-bold mb-4">404 - Not Found</h1>
-        <img src="assets/hover.gif" alt="." className="w-1/2 size-2" />
-        <p className="mb-4">The page you are looking for does not exist</p>
+        <p className="mb-4"><FaRegFaceGrinBeamSweat className="animate-pulse inline-block align-middle"/> The page you are looking for does not exist</p>
         <Link to="/" className="btn btn-outline btn-primary w-full flex items-center justify-center">Home</Link>
         </div>
     </div>
