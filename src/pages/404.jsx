4559866bc1c9905cad5d73d510a0df2add4f4f7a
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-09-08 10:57:11 +0000

    Fix

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -2,14 +2,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaRegFaceGrinBeamSweat } from "react-icons/fa6";
+import { House } from 'lucide-react';
 
 const NotFound = () => {
   return (
-    <div className="container mx-auto p-4">
-      <div className="form-container min-h-screen text-center flex items-center gap-2">
+    <div className="flex items-center justify-center mx-auto p-4">
+      <div className="form-container min-h-screen text-center">
         <h1 className="text-4xl font-bold mb-4">404 - Not Found</h1>
-        <p className="mb-4"><FaRegFaceGrinBeamSweat className="animate-pulse inline-block align-middle"/> The page you are looking for does not exist</p>
-        <Link to="/" className="btn btn-outline btn-primary w-full flex items-center justify-center">Home</Link>
+        <p className="mb-4"><FaRegFaceGrinBeamSweat className="animate-pulse inline-block align-middle mr-1"/> The page you are looking for does not exist</p>
+        <Link to="/" className="btn btn-outline btn-primary w-full"><House className="size-4"/> Home</Link>
         </div>
     </div>
   );
