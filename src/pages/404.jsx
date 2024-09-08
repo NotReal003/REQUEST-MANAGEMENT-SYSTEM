56be9c1d8e65af14864e84e16860416ecd7f0737
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-09-08 10:27:23 +0000

    Foxs

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -8,7 +8,8 @@ const NotFound = () => {
     <div className="container mx-auto p-4">
       <div className="form-container min-h-screen text-center items-center">
         <h1 className="text-4xl font-bold mb-4">404 - Not Found</h1>
-        <p className="mb-4"><FaRegFaceGrinBeamSweat className="inline-block align-middle size-2"/> The page you are looking for does not exist</p>
+        <img src="assets/hover.gif" alt="." className="w-1/2 size-2" />
+        <p className="mb-4">The page you are looking for does not exist</p>
         <Link to="/" className="btn btn-outline btn-primary w-full flex items-center justify-center">Home</Link>
         </div>
     </div>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -93,20 +93,20 @@ const Profile = () => {
           <h2 className="text-xl font-semibold mb-4">Account Details</h2>
           <div className="text-sm gap-2">
             <p>
-              <strong className="mr-2">
-                <MdMarkEmailRead className="inline-block align-middle m-2" /> Email: 
+              <strong className="mr-1">
+                <MdMarkEmailRead className="inline-block align-middle m-1" /> Email: 
               </strong> 
                {user.email}
             </p>
             <p>
-              <strong className="mr-2">
-                <FaCheck className="inline-block align-middle m-2" /> Joined: 
+              <strong className="mr-1">
+                <FaCheck className="inline-block align-middle m-1" /> Joined: 
               </strong> 
                {new Date(user.joinedAt).toLocaleDateString()}
             </p>
             <p>
-              <strong className="mr-2">
-                <FaDiscord className="inline-block align-middle m-2" /> Discord ID: 
+              <strong className="mr-1">
+                <FaDiscord className="inline-block align-middle m-1" /> Discord ID: 
               </strong> 
                {user.id}
             </p>
