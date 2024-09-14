3acd3f336981d5b30c4ef2b2387113d6ba5a2d1f
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-09-14 15:12:29 +0000

    Fics

--- src/components/ForSell.jsx
+++ src/components/ForSell.jsx
@@ -4,7 +4,7 @@ const ForSell = () => {
   return (
     <div className="bg-base-100 shadow-lg text-sm p-4 text-center rounded-lg">
       This website and with backend API are for sell. For more information, please{' '}
-      <a href="discord:/users/1131271104590270606" className="hover:underline font-bold">
+      <a href="discord:/users/1131271104590270606" className="text-blue-500 hover:underline font-bold">
         Contact Me On Discord
       </a>,  if not working, my username in discord is <strong>notnt77</strong> add me there :)
     </div>
