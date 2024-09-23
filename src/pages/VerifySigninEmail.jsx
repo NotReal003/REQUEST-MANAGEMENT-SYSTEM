2f4be6ea7593779223d29f9b994202f253af0c64
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-09-23 15:00:15 +0000

    Fix

--- src/pages/VerifySigninEmail.jsx
+++ src/pages/VerifySigninEmail.jsx
@@ -28,7 +28,7 @@ const VerifySigninEmail = () => {
       }
     };
     signIn();
-  }, [code, token, navigate]);
+  }, [token, navigate]);
 
   return (
     <div className="h-screen flex items-center justify-center bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 max-w-md md:max-w-lg mx-auto shadow-lg rounded-lg">
