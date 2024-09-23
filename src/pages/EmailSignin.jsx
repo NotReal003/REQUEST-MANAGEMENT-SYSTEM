4dcb1d9ec3d0bb314794a56fd3789b1785117b71
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-09-23 15:31:02 +0530

    Update EmailSignin.jsx

--- src/pages/EmailSignin.jsx
+++ src/pages/EmailSignin.jsx
@@ -4,8 +4,9 @@ import { useNavigate } from 'react-router-dom';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const EmailSignin = () => {
+const EmailSignup = () => {
   const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
   const navigate = useNavigate();
 
   const isValidEmail = (email) => {
@@ -13,7 +14,7 @@ const EmailSignin = () => {
     return emailRegex.test(email);
   };
 
-  const handleSignin = async (e) => {
+  const handleSignup = async (e) => {
     e.preventDefault();
     if (!isValidEmail(email)) {
       toast.error('Please enter a valid email address');
@@ -21,13 +22,13 @@ const EmailSignin = () => {
     }
 
     try {
-      await axios.post('https://api.notreal003.xyz/auth/email-signin', { email });
+      await axios.post('https://api.notreal003.xyz/auth/email-signup', { email, username });
       toast.success('Verification email sent! Redirecting...');
       setTimeout(() => {
-        navigate('/verify-signin-email', { state: { email } });
+        navigate('/verify-email', { state: { email } });
       }, 3000);
     } catch (error) {
-      toast.error('There was a problem during signin. Please try again.');
+      toast.error('There was a problem during signup. Please try again.');
     }
   };
 
@@ -35,8 +36,18 @@ const EmailSignin = () => {
     <div className="h-screen flex items-center justify-center bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 max-w-md md:max-w-lg mx-auto shadow-lg rounded-lg">
       <ToastContainer />
       <div className="bg-gradient-to-br from-black-400 via-black-500 to-black-600 p-8 bg-opacity-10 rounded-lg shadow-lg max-w-sm ml-2 mr-2 m-2 w-full">
-        <h1 className="text-4xl font-bold mb-6 text-center text-white">Sign In with Email</h1>
-        <form onSubmit={handleSignin}>
+        <h1 className="text-4xl font-bold mb-6 text-center text-white">Sign Up with Email</h1>
+        <form onSubmit={handleSignup}>
+          <div className="mb-4">
+            <input
+              type="text"
+              placeholder="Username"
+              className="input input-bordered w-full"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
           <div className="mb-4">
             <input
               type="email"
@@ -48,7 +59,7 @@ const EmailSignin = () => {
             />
           </div>
           <button type="submit" className="btn btn-primary w-full">
-            Sign In
+            Sign Up
           </button>
         </form>
       </div>
@@ -56,4 +67,4 @@ const EmailSignin = () => {
   );
 };
 
-export default EmailSignin;
+export default EmailSignup;
