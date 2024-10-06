3a498b6ae449f455115ebe86ae807bc01b7449d2
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-10-06 16:28:43 +0000

    Gb

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,16 +9,6 @@ const Login = () => {
       <div className="bg-gradient-to-br from-black-400 via-black-500 to-black-600 p-8 bg-opacity-10 rounded-lg shadow-lg max-w-sm ml-2 mr-2 m-2 w-full">
         <h1 className="text-xl font-bold mb-4 text-center text-white">Login</h1>
         <p className="text-center text-white mb-8">Choose a login method to continue.</p>
-        <div className="tooltip tooltip-info w-full" data-tip="GitHub Login is currently not accepted.">
-          <button
-            disabled
-            className="btn btn-outline btn-primary w-full mt-5 flex items-center justify-center transition-all duration-200 no-animation"
-          >
-            <IoLogoGithub />
-            Login with GitHub
-          </button>
-        </div>
-
         <button
           onClick={() => window.location.href = `https://api.notreal003.xyz/auth/signin`}
           className="btn btn-outline btn-primary w-full mt-5 flex items-center justify-center transition-all duration-200 no-animation hover:bg-primary hover:border-primary hover:text-white"
@@ -31,6 +21,15 @@ const Login = () => {
           <FaEnvelope />
           Login with Email
         </Link>
+        <div className="tooltip tooltip-info w-full" data-tip="GitHub Login is currently not accepted.">
+          <button
+            disabled
+            className="btn btn-outline btn-primary w-full mt-5 flex items-center justify-center transition-all duration-200 no-animation"
+          >
+            <IoLogoGithub />
+            Login with GitHub
+          </button>
+        </div>
 
         <p className="text-center text-white mt-5">
           Don’t have an account?{" "}
