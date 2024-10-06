4646144d15a1353587e8249f3aeba36d2e05c25a
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-10-06 15:22:25 +0000

    Fixs

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaDiscord, FaEnvelope, Fq } from "react-icons/fa6";
+import { FaDiscord, FaEnvelope } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
 import { IoLogoGithub } from "react-icons/io";
 
@@ -9,14 +9,15 @@ const Login = () => {
       <div className="bg-gradient-to-br from-black-400 via-black-500 to-black-600 p-8 bg-opacity-10 rounded-lg shadow-lg max-w-sm ml-2 mr-2 m-2 w-full">
         <h1 className="text-xl font-bold mb-4 text-center text-white">Login</h1>
         <p className="text-center text-white mb-8">Choose a login method to continue.</p>
-
-        <button
-          onClick={() => window.location.href = `https://api.notreal003.xyz/auth/github`}
-          className="btn btn-outline btn-primary w-full mt-5 flex items-center justify-center transition-all duration-200 no-animation hover:bg-primary hover:border-primary hover:text-white"
-        >
-          <IoLogoGithub />
+        <div className="tooltip tooltip-error w-full" data-tip="GitHub sign-in is not currently accepted">
+          <button
+            disabled
+            className="btn btn-outline btn-primary w-full mt-5 flex items-center justify-center transition-all duration-200 no-animation"
+          >
+            <IoLogoGithub />
             Login with GitHub
           </button>
+        </div>
 
         <button
           onClick={() => window.location.href = `https://api.notreal003.xyz/auth/signin`}
